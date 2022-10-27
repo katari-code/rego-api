52be1a0e68f75f1dfd1022fc83ff33afd3f905c8
dev-katari <elsaid@live.utm.my>
2022-10-28 04:53:29 +0700

    versioning the API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
+
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
